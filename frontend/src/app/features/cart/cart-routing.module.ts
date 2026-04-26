aac6d8a81610783fe2e84a7d409907ec71775bc0
luntarex <kazancioglumuratt@gmail.com>
2026-04-26 17:02:37 +0300

    Cart feature implemented

--- frontend/src/app/features/cart/cart-routing.module.ts
+++ frontend/src/app/features/cart/cart-routing.module.ts
@@ -1,1 +1,11 @@
-﻿export class CartRoutingModuleRoutingModule {}
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartView } from './cart-view/cart-view';
+
+const routes: Routes = [{ path: '', component: CartView }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CartRoutingModule {}
