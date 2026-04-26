c5eeab48272537d1231a7c60bbb23d56e8a53ba1
luntarex <kazancioglumuratt@gmail.com>
2026-04-26 13:27:07 +0300

    product-list, product-detail and product-form features implemented

--- frontend/src/app/features/products/product-form/product-form.spec.ts
+++ frontend/src/app/features/products/product-form/product-form.spec.ts
@@ -1,1 +1,23 @@
-﻿describe('product-form', () => { it('should create', () => { expect(true).toBeTrue(); }); });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductForm } from './product-form';
+
+describe('ProductForm', () => {
+  let component: ProductForm;
+  let fixture: ComponentFixture<ProductForm>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProductForm]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProductForm);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
