92fa52ac6a0c9dd9e25edd52dbbc16a2981e2867
luntarex <kazancioglumuratt@gmail.com>
2026-04-26 17:10:05 +0300

    Analytics page implemented

--- frontend/src/app/features/analytics/customer-segmentation/customer-segmentation.ts
+++ frontend/src/app/features/analytics/customer-segmentation/customer-segmentation.ts
@@ -1,1 +1,11 @@
-﻿export class CustomerSegmentation {}
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-segmentation',
+  imports: [],
+  templateUrl: './customer-segmentation.html',
+  styleUrl: './customer-segmentation.css',
+})
+export class CustomerSegmentation {
+
+}
