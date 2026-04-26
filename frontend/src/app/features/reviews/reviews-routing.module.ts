9bea40a26c48e20b8c58120d06d9982f365c0c21
luntarex <kazancioglumuratt@gmail.com>
2026-04-26 17:12:42 +0300

    Reviews page implemented(for corporate and admin)

--- frontend/src/app/features/reviews/reviews-routing.module.ts
+++ frontend/src/app/features/reviews/reviews-routing.module.ts
@@ -1,1 +1,19 @@
-﻿export class ReviewsRoutingModuleRoutingModule {}
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ReviewList } from './review-list/review-list';
+import { RoleGuard } from '../../core/guards/role.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ReviewList,
+    canActivate: [RoleGuard],
+    data: { roles: ['INDIVIDUAL', 'CORPORATE', 'ADMIN'] },
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ReviewsRoutingModule { }
--- frontend/src/app/features/reviews/reviews.module.ts
+++ frontend/src/app/features/reviews/reviews.module.ts
@@ -1,1 +1,9 @@
-﻿export class ReviewsModule {}
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReviewsRoutingModule } from './reviews-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ReviewsRoutingModule]
+})
+export class ReviewsModule { }
